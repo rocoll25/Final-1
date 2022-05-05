7b159e9e47d9da1a5163b0266a50250f3e13ec1a
coll25 <104849401+rocoll25@users.noreply.github.com>
2022-05-05 14:26:06 +0000

    Deleted that stuff because it didn't work

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "SQLiteDb.hpp"
 
 int main() {
-  SQLiteDb db;
-  db.connect("./main.db");
-  std::cerr << db.execStatement(".print hello world") 
-            << db.execStatement(".print goodbye world");
+  std::cout << "Hello World!\n";
 }
