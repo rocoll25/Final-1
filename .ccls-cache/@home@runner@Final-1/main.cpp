db9367395fd6b1588161e2ad615022f8be3b8f73
coll25 <104849401+rocoll25@users.noreply.github.com>
2022-05-05 13:42:23 +0000

    Created file for plan of code

--- .ccls-cache/@home@runner@Final-1/main.cpp
+++ .ccls-cache/@home@runner@Final-1/main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-// INFORMATION FROM THE USER USED TO CREATE THE DATABASE
-std::string u_rows[1000];
-std::string u_columns[25];
-std::string u_title;
-
 int main() {
   std::cout << "Hello World!\n";
 }
